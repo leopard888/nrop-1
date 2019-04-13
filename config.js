a3cfbcf06318a6ad3053bcfc1bf3edf03fd96ebf
lang999 <39425761+lang999@users.noreply.github.com>
2019-04-13 19:20:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   port : 3002 , 
   host : {
     'y66t':'aHR0cDovL3d3dy50NjZ5LmNvbS8=',
-    'nrop19':'aHR0cDovL3d3dy45MXBvcm4uY29tLw==',
+    'nrop19':'aHR0cHM6Ly9tLnQ5ay5zcGFjZQ==',
     'soedivx':'aHR0cHM6Ly93d3cueHZpZGVvcy5jb20v'
   },
 
